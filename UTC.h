b1d4763e760fe943c7a209ef04f981ccae64ae4c
zzl <zhouzhaolin@cug.edu.cn>
2021-03-20 00:19:59 +0800

    V1.0 UTC2JMD

---
+++ UTC.h
@@ -0,0 +1,113 @@
+#pragma once
+#include <iostream>
+#include <ctime>
+class UTC{
+ private:
+  struct __S_UTC{
+  explicit __S_UTC(
+	       unsigned int year =0,
+	       unsigned int mon =0,
+	       unsigned int day =0,
+	       unsigned int hor =0,
+	       unsigned int min =0,
+	       unsigned int sec =0) :
+    m_year(year),m_mon(mon),m_day(day),
+    m_hour(hor),m_min(min),m_sec(sec)  
+    {};
+  unsigned int m_year;
+  unsigned int m_mon;
+  unsigned int m_day;
+  unsigned int m_hour;
+  unsigned int m_min;
+  unsigned int m_sec;
+};
+ public:
+  friend std::ostream& operator << (std::ostream& os, UTC& utc);
+  explicit UTC() : u(new __S_UTC){  };
+  explicit UTC(unsigned int year,
+	       unsigned int mon,
+	       unsigned int day) : u(new __S_UTC{year,mon,day}){  };
+  UTC(const UTC& other){
+    initialize(other);
+  }
+  UTC(UTC&& other){
+    u = other.u;
+    other.u = nullptr;
+  
+  }
+  UTC& operator=(const UTC& other){
+    initialize(other);
+    return *this;
+  };
+  UTC& operator=(UTC&& other){
+    u = other.u;
+    other.u = nullptr;
+    return *this;
+  };
+  void initialize(const UTC& other){
+    if(&other == this)
+      return ;
+    delete u;
+    u = new __S_UTC(other.u->m_year,other.u->m_mon,other.u->m_day,other.u->m_hour,other.u->m_min,other.u->m_sec);
+  }
+  void AutoSetUTC(){
+    time_t now =time(0);
+    char* data = ctime(&now);
+    tm* gm =gmtime(&now);
+    setUTC(gm->tm_year+1900,gm->tm_mon+1,gm->tm_mday);
+  };
+  void setUTC( unsigned int year,
+	       unsigned int mon,
+	       unsigned int day,
+	       unsigned int hor =0,
+	       unsigned int min =0,
+	       unsigned int sec =0){
+    u->m_year = year;
+    u->m_mon =mon;
+    u->m_day =day;
+  };
+  float utc2jd();
+  float utc2mjd();
+  static UTC  mjd2utc(float mjd);
+  
+
+  
+  virtual ~UTC(){
+    delete u;
+  };
+ private:
+  __S_UTC* u;
+};
+float UTC::utc2jd(){
+  return 367*(u->m_year) - 7.0*(u->m_year+(u->m_mon+9.0)/12)/4+275*u->m_mon/9.0+u->m_day+1721014;
+}
+float UTC::utc2mjd(){
+  return utc2jd() - 2400000.5;
+}
+
+std::ostream& operator << (std::ostream& os, UTC& utc){
+  std::cout <<(utc.u)->m_year<<" "
+     <<utc.u->m_mon<<" "
+     <<utc.u->m_day<<" "
+      <<utc.u->m_hour<<" "
+     <<utc.u->m_min<<" "
+     <<utc.u->m_sec<<std::endl;
+  return os;
+}
+
+UTC UTC::mjd2utc(float mjd)
+{
+  int y1,m1,k;
+  y1=(int)( (mjd-15078.2)/365.25 );
+  m1=(int)((mjd-14956.1-(int)(y1*365.25))/30.6001); 
+  int day =(mjd-14956-(int)(y1*365.25)-(int)(m1*30.6001)); 
+  k=0;
+  if(y1==14 || y1==15) k=1;
+  int mon = (int)(m1-1-k*12);
+  k=0;
+  if(y1==14 || y1==15) k=1;
+  int year = (int)(y1+k);
+  //std::cout << year<<" "<<mon<<" "<<day<<std::endl;
+  return UTC(year,mon,day);
+  
+}
--- UTC2MJD.cpp
+++ UTC2MJD.cpp
@@ -1,81 +1,14 @@
-#pragma once
 #include<iostream>
-#include<ctime>
-using namespace std;
-//static const char mon_name[][4] = {
-//    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
-//    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
-//  };
-struct UTC{
-  explicit UTC(
-	       unsigned int year,
-	       unsigned int mon,
-	       unsigned int day,
-	       unsigned int hor =0,
-	       unsigned int min =0,
-	       unsigned int sec =0) :
-    m_year(year),m_mon(mon),m_day(day),
-    m_hour(hor),m_min(min),m_sec(sec)  
-    {};
-  friend ostream& operator << (ostream& os, UTC& u);  
-  unsigned int m_year =0;
-  unsigned int m_mon  =0;
-  //  unsigned int m_week =0;
-  unsigned int m_day  =0;
-  unsigned int m_hour =0;
-  unsigned int m_min  =0;
-  unsigned int m_sec  =0;
-};
-
-ostream& operator << (ostream& os, UTC& u){
-  cout <<u.m_year<<" "
-       <<u.m_mon<<" "
-       <<u.m_week<<" "
-       <<u. m_day<<" "
-       <<u.m_hour<<" "
-       <<u.m_min<<" "
-       <<u. m_sec<<endl;
-  return os;
-}
-//utc->jd/mjd
-float utc2jd(UTC& u){
-  return 367*(u.m_year) - 7.0*(u.m_year+(u.m_mon+9.0)/12)/4+275*u.m_mon/9.0+u.m_day+1721014;
-}
-float utc2mjd(UTC& u){
-  return utc2jd(u) - 2400000.5;
-}
-
-//mjd->utc
-void mjd2utc(float mjd)
-{
-int y1,m1,k;
-  y1=(int)( (mjd-15078.2)/365.25 );
-  m1=(int)((mjd-14956.1-(int)(y1*365.25))/30.6001); 
-  int day =(mjd-14956-(int)(y1*365.25)-(int)(m1*30.6001)); 
-  k=0;
-  if(y1==14 || y1==15) k=1;
-  int mon = (int)(m1-1-k*12);
-  k=0;
-  if(y1==14 || y1==15) k=1;
-  int year = (int)(y1+k);
-  cout << year<<" "<<mon<<" "<<day<<endl;
-}
+#include "UTC.h"
 
 int main(){
-	time_t now =time(0);
-	char* data = ctime(&now);
-	tm* gm =gmtime(&now);
-	//UTC
-	UTC utc(gm->tm_year+1900,
-		gm->tm_mon+1,
-		gm->tm_mday,
-		gm->tm_hour,
-		gm->tm_min,
-		gm->tm_sec);
-	cout << utc;
-	//	cout <<utc2jd(utc)<<endl;
-	cout <<utc2mjd(utc)<<endl;
-	cout <<"======"<<endl;
-	mjd2utc(utc2mjd(utc));
-	return 0;
+  UTC utc;
+  utc.AutoSetUTC();
+  std::cout << utc;
+  std::cout << utc.utc2jd()<<std::endl;
+  std::cout <<  utc.utc2mjd()<<std::endl;
+  std::cout <<"======"<<std::endl;
+  utc = UTC::mjd2utc(utc.utc2mjd());
+  std::cout << utc;
+  return 0;
 }
