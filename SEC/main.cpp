b80a75404208914cd7b8e66b1a7dc8ae513752cf
zzl <zhouzhaolin@cug.edu.cn>
2021-03-25 00:31:41 +0800

    V 6.0

--- SEC/main.cpp
+++ SEC/main.cpp
@@ -4,26 +4,34 @@
 #include <iostream>
 using namespace date;
 using namespace std::chrono;
-constexpr auto jdiff()
-{
-   return sys_days{jan/1/1970} - (sys_days{nov/24/-4713} + 12h);
+using dh = std::chrono::duration<double, std::ratio<84600>>;
+
+constexpr auto jdiff() {
+  return duration_cast<dh>(sys_days{jan / 1 / 1970} -
+                           (sys_days{nov / 24 / -4713} + 12h));
 }
-utc_time<std::chrono::seconds> MJD2UTC(double MJD)
-{
-  using dh = std::chrono::duration<double, std::ratio<84600>>;
-  utc_time<seconds> no_leap{round<seconds>(dh{ MJD + 2400000.5 - dh{jdiff()}.count()})};
-  auto ut_lp = get_leap_second_info(no_leap);
-  return utc_time<seconds>{round<seconds>(dh{  dh{ut_lp.elapsed}.count() + MJD + 2400000.5 - dh{jdiff()}.count()})};
+template <typename Duration> double UTC2MJD(date::utc_time<Duration> &ut) {
+  return duration_cast<dh>(ut.time_since_epoch()).count() + jdiff().count() -
+         duration_cast<dh>(get_leap_second_info(ut).elapsed).count() -
+         2400000.5;
 }
 
+date::utc_time<seconds> MJD2UTC(double MJD) {
+  seconds utcWithoutLeap =
+      duration_cast<seconds>(dh(MJD) + dh(2400000.5) - jdiff());
+  utc_time<seconds> no_leap{utcWithoutLeap};
+  return utc_time<seconds>{
+      duration_cast<seconds>(dh{get_leap_second_info(no_leap).elapsed}) +
+      utcWithoutLeap};
+}
 int main(){
   //set utc_time 
-  utc_time<seconds> utc_s{ round<seconds>(utc_clock::now().time_since_epoch()) };
+  utc_time<seconds> utc_s{ duration_cast<seconds>(utc_clock::now().time_since_epoch()) };
   std::cout << "UTC - > SEC" << std::endl;
   std::cout << utc_s.time_since_epoch().count() <<"s"<< std::endl;
   std::cout << "MJD - > SEC" << std::endl;
   utc_s = MJD2UTC(10000.0);
   std::cout << utc_s.time_since_epoch().count() <<"s"<< std::endl;
-  
+ 
   return 0;
 }
--- UTC2JD_MJD/main.cpp
+++ UTC2JD_MJD/main.cpp
@@ -2,46 +2,36 @@
 #include <chrono>
 #include <cstdio>
 #include <iostream>
+using namespace std::chrono_literals;
+using namespace std::chrono;
+using namespace date;
+using dh = std::chrono::duration<double, std::ratio<84600>>;
 
-constexpr auto jdiff()
-{
-  
-   using namespace date;
-   using namespace std::chrono_literals;
-   return sys_days{jan/1/1970} - (sys_days{nov/24/-4713} + 12h);
+constexpr auto jdiff() {
+  return duration_cast<dh>(sys_days{jan / 1 / 1970} -
+                           (sys_days{nov / 24 / -4713} + 12h));
+}
+template <typename Duration> double UTC2MJD(date::utc_time<Duration> &ut) {
+  return duration_cast<dh>(ut.time_since_epoch()).count() + jdiff().count() -
+         duration_cast<dh>(get_leap_second_info(ut).elapsed).count() -
+         2400000.5;
 }
 
-template<typename Duration>
-double UTC2MJD(date::utc_time<Duration>& ut)
-{
-  using dh = std::chrono::duration<double, std::ratio<84600>>;
-  using namespace std::chrono;
-  using namespace date;
-  utc_time<seconds> ut_now{round<seconds>(utc_clock::now().time_since_epoch())};
-  auto ut_lp = get_leap_second_info(ut);
-  return dh{ut.time_since_epoch()}.count() + dh{jdiff()}.count() - dh{ut_lp.elapsed}.count() - 2400000.5;
+date::utc_time<seconds> MJD2UTC(double MJD) {
+  seconds utcWithoutLeap =
+      duration_cast<seconds>(dh(MJD) + dh(2400000.5) - jdiff());
+  utc_time<seconds> no_leap{utcWithoutLeap};
+  return utc_time<seconds>{
+      duration_cast<seconds>(dh{get_leap_second_info(no_leap).elapsed}) +
+      utcWithoutLeap};
 }
 
-
-date::utc_time<std::chrono::seconds> MJD2UTC(double MJD)
-{
-  using dh = std::chrono::duration<double, std::ratio<84600>>;
-  using namespace std::chrono;
-  using namespace date;
-  utc_time<seconds> no_leap{round<seconds>(dh{ MJD + 2400000.5 - dh{jdiff()}.count()})};
-  auto ut_lp = get_leap_second_info(no_leap);
-  return utc_time<seconds>{round<seconds>(dh{  dh{ut_lp.elapsed}.count() + MJD + 2400000.5 - dh{jdiff()}.count()})};
-}
-
-int main(){
-  using namespace date;
-  using namespace std::chrono;
+int main() {
   printf("UTC -> MJD\n");
-  using dh = std::chrono::duration<double, std::ratio<84600>>;
   utc_time<dh> ut(utc_clock::now().time_since_epoch());
-  printf("%lf\n", UTC2MJD(ut) );
+  printf("%lf\n", UTC2MJD(ut));
   printf("MJD -> UTC\n");
   std::cout << MJD2UTC(UTC2MJD(ut)) << std::endl;
-  
+
   return 0;
 }
